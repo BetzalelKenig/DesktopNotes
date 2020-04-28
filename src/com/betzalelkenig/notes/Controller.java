aebaf9615e4825ec9a7c2998b0d508a15dba78bc
Betzalel Kenig <betzalelkenig@gmail.com>
2020-04-28 14:10:03 +0300

    conact the list view

--- src/com/betzalelkenig/notes/Controller.java
+++ src/com/betzalelkenig/notes/Controller.java
@@ -1,4 +1,38 @@
 package com.betzalelkenig.notes;
 
+import datamodel.Note;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
+
+    private List<Note> notes;
+    @FXML
+    private ListView noteListView;
+
+    public void initialize(){
+        Note item1 = new Note("Java", "JavaFX and other proj",
+                LocalDate.of(2020, Month.MAY,1));
+
+        Note item2 = new Note("Linux", "toturial and shell in cw",
+                LocalDate.of(2020, Month.MAY,20));
+        Note item3 = new Note("crypto", "wiki & cw & math",
+                LocalDate.of(2020, Month.APRIL,1));
+
+        notes = new ArrayList<Note>();
+        notes.add(item1);
+        notes.add(item2);
+        notes.add(item3);
+
+        noteListView.getItems().setAll(notes);
+        noteListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
+
+
 }
