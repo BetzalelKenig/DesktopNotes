13c26336631a4077a438f82a539db971749e4ed3
Betzalel Kenig <betzalelkenig@gmail.com>
2020-04-28 14:19:51 +0300

    show short description

--- src/datamodel/Note.java
+++ src/datamodel/Note.java
@@ -37,4 +37,9 @@ public class Note {
     public void setDeadline(LocalDate deadline) {
         this.deadline = deadline;
     }
+
+    @Override
+    public String toString() {
+        return shortDescription;
+    }
 }
